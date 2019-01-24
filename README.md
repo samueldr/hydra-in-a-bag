2323a9fc57b507f78767952df3e5ebde6c965402
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-01-23 21:16:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+`hydra-in-a-bag`
+================
+
+This project aims to provide a one-click^W command solution to running a hydra
+instance **for development purposes**. Thus the name, this is not a
+ready-to-use production environment, but a only a convenience thing.
+
+This also aims to isolate the development environment maximally, and to ensure
+the end-user does not need to change any system setting.
+
+This is mainly geared towards development on the web-facing parts of hydra.
+
+Security
+--------
+
+Assume close to none. First, you're running your own development code! Then,
+this runs an UNPROTECTED database for convenience. Then, this defaults to the
+development runners, when possible.
+
+Requirements
+------------
+
+  * `nix`, only tested under the 2.x `nix` series.
+
+Usage
+-----
+
+```
+~ $ mkdir -p $HOME/Projects/hydra
+~ $ cd $HOME/Projects/hydra
+~/Projects/hydra $ git clone https://github.com/NixOS/hydra.git
+~/Projects/hydra $ git clone https://github.com/samueldr/hydra-in-a-bag.git
+~/Projects/hydra $ cd hydra-in-a-bag
+~/Projects/hydra/hydra-in-a-bag $ nix-shell --run init-database
+~/Projects/hydra/hydra-in-a-bag $ nix-shell --run start
+```
+
+Then visit http://localhost:3000/
---
+++ shell.nix
@@ -0,0 +1,77 @@
+{
+  pkgs ? import <nixpkgs> {}
+  , hydraDir ? toString ../hydra
+  , bagDir ? toString ./.
+}:
+
+let
+  inherit (pkgs) system writeText;
+  release = import "${hydraDir}/release.nix" { shell = true; };
+
+  waitForPg = ''(until echo '\q' | psql -U hydra; do sleep 1; done)'';
+
+  initProcfile = writeText "hydra-in-a-bag-Procfile" ''
+    db:         exec bash ${bagDir}/dev-database
+    # Ugly, this sleeps after the first posgresql connection only to ensure
+    # the pg_setup parts of dev-database had time to init stuff properly.
+    init:       ${waitForPg}; sleep 10; exec hydra-init
+  '';
+
+  procfile = writeText "hydra-in-a-bag-Procfile" ''
+    db:         exec bash ${bagDir}/dev-database
+    web:        ${waitForPg}; exec hydra-dev-server --debug --restart
+    evaluator:  ${waitForPg}; exec hydra-evaluator
+    queue:      ${waitForPg}; exec hydra-queue-runner
+  '';
+
+  envfile = writeText "hydra-in-a-bag-env" ''
+    HYDRA_HOME=${hydraDir}/src
+    HYDRA_DATA=${bagDir}/_data
+    HYDRA_CONFIG=${bagDir}/_data/hydra.conf
+    HYDRA_DBI="dbi:Pg:dbname=hydra;host=localhost;user=hydra;"
+  '';
+in
+  release.build.${system}.overrideAttrs(old: {
+  buildInputs = with pkgs; [
+    foreman
+  ] ++ old.buildInputs;
+
+  shellHook = ''
+    # Oof, this dance is weird, but this is because the hydra development shell hook
+    # assumes $(pwd) is the hydra directory.
+    _pwd=$PWD
+    cd ${hydraDir}
+    ${old.shellHook}
+    cd $_pwd
+    unset _pwd
+
+    make-hydra() {
+      set -e
+      (
+      echo ":: Building hydra"
+      cd ${hydraDir}
+      ./bootstrap
+      configurePhase
+      make
+      echo ":: Done building hydra"
+      )
+      set +e
+    }
+    dev-database() {
+      (
+      source ${envfile}
+      exec bash ${bagDir}/dev-database
+      )
+    }
+    init-database() {
+      make-hydra
+      echo ":: Database initialisation"
+      foreman start -e ${envfile} -d ${bagDir} -f ${initProcfile}
+    }
+    start() {
+      make-hydra
+      echo ":: Starting hydra"
+      exec foreman start -e ${envfile} -d ${bagDir} -f ${procfile}
+    }
+  '';
+})
